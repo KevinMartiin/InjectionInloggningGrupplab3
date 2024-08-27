2c273f7b537bdb9b1310d46f75e8ffa55a2b3be0
kevinmartiin <nextgenallu@hotmail.com>
2024-08-27 11:55:42 +0200

    Add project files.

---
+++ InjectionInloggning/MySqlDatabaseService.cs
@@ -0,0 +1,47 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace InjectionInloggning
+{
+    public class MySqlDatabaseService : IDatabaseService
+    {
+        private readonly string _connectionString;
+
+        public MySqlDatabaseService(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public bool AuthenticateUser(string username, string password)
+        {
+            using (var conn = new MySqlConnection(_connectionString))
+            {
+                string sqlQuery = "SELECT * FROM users WHERE username = @username AND password = @password;";
+                using (var cmd = new MySqlCommand(sqlQuery, conn))
+                {
+                    cmd.Parameters.AddWithValue("@username", username);
+                    cmd.Parameters.AddWithValue("@password", password);
+
+                    try
+                    {
+                        conn.Open();
+                        using (var reader = cmd.ExecuteReader())
+                        {
+                            return reader.Read();
+                        }
+                    }
+                    catch
+                    {
+                        // Hantera undantag om det behövs
+                        throw;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ InjectionInloggning/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InjectionInloggning
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Form1());
+            string connectionString = ConfigurationManager.ConnectionStrings["MySqlConnection"].ConnectionString;
+
+            // Skapa instansen av IDatabaseService med connectionString från App.config
+            IDatabaseService databaseService = new MySqlDatabaseService(connectionString);
+
+            // Skicka instansen till Form1
+            Application.Run(new Form1(databaseService));
+        }
+    }
+}
